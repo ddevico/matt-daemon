164a145ce27eb35fe80ba3a0ad07159ba755e71a
ddevico <ddevico@student.42.com>
2018-01-10 11:56:35 +0100

    test:

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ddevico <ddevico@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 15:07:11 by ddevico           #+#    #+#             */
-/*   Updated: 2018/01/10 11:55:45 by davydevico       ###   ########.fr       */
+/*   Updated: 2018/01/10 11:56:29 by davydevico       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -232,23 +232,23 @@ void run(int fd)
 {
 	if (chdir("/") == -1)
 	{
-		reporter->print_log("Can't chdir to /");
+		reporter->print_log("ERROR", "Can't chdir to /");
 		exit(EXIT_FAILURE);
 	}
 	if (setsid() == -1)
 	{
-		reporter->print_log("Can't setsid");
+		reporter->print_log("ERROR", "Can't setsid");
 		exit(EXIT_FAILURE);
 	}
 	int nullop = open("/dev/null", O_RDWR);
 	if (nullop == -1)
 	{
-		reporter->print_log("Can't open /dev/null");
+		reporter->print_log("ERROR", "Can't open /dev/null");
 		exit(EXIT_FAILURE);
 	}
 	if (dup2(nullop, 0) == -1 || dup2(nullop, 1) == -1 || dup2(nullop, 2) == -1)
 	{
-		reporter->print_log("can't redirect stdin/stdout/stderr to /dev/null");
+		reporter->print_log("ERROR", "can't redirect stdin/stdout/stderr to /dev/null");
 		exit(EXIT_FAILURE);
 	}
 	listen();
